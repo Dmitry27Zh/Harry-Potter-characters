6ee4905afd9275de1dfa702b66ee16a66d9572a4
Dmitry <dmitry27zh@gmail.com>
2022-09-03 18:32:10 +0300

    Load and display characters

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const URL = 'http://hp-api.herokuapp.com/api/characters'
+const charactersList = document.getElementById('characters-list')
+
+const loadCharacters = async () => {
+  try {
+    const res = await fetch(URL)
+    const characters = await res.json()
+    displayCharacters(characters)
+    console.table(characters)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+loadCharacters()
+
+const displayCharacters = (characters) => {
+  const htmlString = characters
+    .map(({ name, house, image }) => {
+      const houseString = house ? `<p>House: ${house}</p>` : ''
+
+      return `
+        <li class="character">
+          <h3>${name}</h3>
+          ${houseString}
+          <img src="${image}" />
+        </li>
+      `
+    })
+    .join('')
+
+  charactersList.innerHTML = htmlString
+}
